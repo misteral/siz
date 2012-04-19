a40c7d5ee5f9358161c215bc5263174653ebb428
misteral <mister-al@ya.ru>
2012-04-19 09:13:27 +0600

    Добавлена валидация на должности и положенные СИЗ

--- app/models/dolgnost.rb
+++ app/models/dolgnost.rb
@@ -2,4 +2,6 @@ class Dolgnost < ActiveRecord::Base
   has_many :rabotniks
   has_many :pologenos , :dependent => :destroy
   accepts_nested_attributes_for :pologenos, :reject_if => :all_blank
+  validates :name, :presence => true
+  validates_associated :pologenos
 end

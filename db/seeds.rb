0645ab1c63d7eec1bd52b5e56fb3bb6f4f19b82a
misteral <mister-al@ya.ru>
2012-04-23 08:30:42 +0600

    Исправил seed кавычек не было

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 # encoding: utf-8
 
-  Pol.create([{name: 'Муж.'}, name: 'Жен.'])
+  Pol.create([{name: 'Муж.'}, {name: 'Жен.'}])
   TipDoc.create([{name: "Приход на склад"}, {name: 'Списание на работника'}, {name: 'Списание с работника'}])

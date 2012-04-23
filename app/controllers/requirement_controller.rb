e25362c5b922f2dc77f39eb8a968140fe6d90ba7
misteral <mister-al@ya.ru>
2012-04-23 17:09:32 +0600

    Начал делать контроллер

--- app/controllers/requirement_controller.rb
+++ app/controllers/requirement_controller.rb
@@ -1,5 +1,8 @@
 class RequirementController < ApplicationController
   def index
-
+    respond_to do |format|
+      format.html  # index.html.erb
+      #format.json  { render :json => @posts }
+    end
   end
 end

c8d8e971ca31ad2d751aa5ebeee8164774b79d38
misteral <mister-al@ya.ru>
2012-04-25 12:35:37 +0600

    Добавил календарик на страницу даты

--- app/controllers/requirement_controller.rb
+++ app/controllers/requirement_controller.rb
@@ -1,5 +1,11 @@
 class RequirementController < ApplicationController
+
   def index
+    if :params['date_reg']
+      @date_reg = :params['date_reg']
+    else
+      @date_reg = ""
+    end
     respond_to do |format|
       format.html  # index.html.erb
       #format.json  { render :json => @posts }

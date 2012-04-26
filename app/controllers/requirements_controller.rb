ea6ec2af93cdb9223c66d875f495ee2c21d7013c
misteral <mister-al@ya.ru>
2012-04-26 16:21:28 +0600

    Подготовка данных для таблицы во вьюхе пока не получается

--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -9,13 +9,18 @@ class RequirementsController < ApplicationController
         needly =(dop_month.month.since(@req_fields[key]['updated_at'])).to_date
         @req_fields[key]['potr'] = ( date_reg > needly ) ? 1:0 #если дата в форме больше вычисленно значит есть потребность
       end
-      @req_fields.group_by(&:siz_id).group_by(&:rost_id).map do |k,v|
-        puts "d"
-      end
+      result = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = {}}}
+      @req_fields.group_by(&:razmer_od_id).each do |k1,v1|
+        v1.each do |razm_id,siz_id|
+          rost_id = razm_id.rost_id
+         result[rost_id.siz_id]
+        end
     end
     respond_to do |format|
       format.html  # index.html.erb
       #format.json  { render :json => @posts }
     end
+    end
   end
 end
+

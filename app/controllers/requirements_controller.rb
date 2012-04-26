1f08f34714a99cbb0518bd3a8ac5615ea0a2d555
misteral <mister-al@ya.ru>
2012-04-26 14:04:16 +0600

    Подготовка данных для таблицы во вьюхе

--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -3,7 +3,15 @@ class RequirementsController < ApplicationController
   def index
     if params['date_reg']
       date_reg = Date.parse(params['date_reg'])
-      @tbl_fields = DocTable.requirements(date_reg)
+      @req_fields = DocTable.requirements.all
+      @req_fields.each_index do |key|
+        dop_month = @req_fields[key]['nagod']*12
+        needly =(dop_month.month.since(@req_fields[key]['updated_at'])).to_date
+        @req_fields[key]['potr'] = ( date_reg > needly ) ? 1:0 #если дата в форме больше вычисленно значит есть потребность
+      end
+      @req_fields.group_by(&:siz_id).group_by(&:rost_id).map do |k,v|
+        puts "d"
+      end
     end
     respond_to do |format|
       format.html  # index.html.erb
--- app/models/doc_table.rb
+++ app/models/doc_table.rb
@@ -20,12 +20,14 @@ class DocTable < ActiveRecord::Base
     group('doc_tables.razmer_od_id, doc_tables.rost_id, doc_tables.siz_id ')
   end
 =end
+=begin
   def self.req(date_p)  #список номенклатуры потребность на дату
-    select("doc_tables.*, pologenos.nagod, IF(DATEADD(doc_tables.updated_at, INTERVAL (12*pologenos.nagod) MONTH) < #{date_p}, 1, 0)").
+    select("doc_tables.*, pologenos.nagod, IF(date(doc_tables.updated_at, +(12*pologenos.nagod) month ) < #{date_p}, 1, 0)").
     joins(:doc).where('docs.utv'=>true,'docs.tip_doc_id' => 2).
     joins(:siz).
     joins(:rabotnik =>:pologenos).where('pologenos.siz_id = sizs.id')
   end
+=end
 
   def self.requirements #определение потребности
     select('doc_tables.*, pologenos.nagod').
